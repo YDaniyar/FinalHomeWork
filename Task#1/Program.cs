c79fac70f35ee9799bf05b26b51a14e9ff7dd5de
Yeren Daniyar <y.daniyar.official@gmail.com>
2024-02-29 00:00:37 +0600

    Task#1

---
+++ Task#1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string input = Console.ReadLine()!;
+        string[] originalArray = input.Split(',');
+
+        string[] newArray = FilterArray(originalArray);
+
+        Console.WriteLine("Новый массив:");
+        foreach (string str in newArray)
+        {
+            Console.Write($"{str.Trim()} ");
+        }
+    }
+
+    static string[] FilterArray(string[] arr)
+    {
+        int count = 0;
+        foreach (string str in arr)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] result = new string[count];
+        int index = 0;
+        foreach (string str in arr)
+        {
+            if (str.Length <= 3)
+            {
+                result[index] = str;
+                index++;
+            }
+        }
+
+        return result;
+    }
+}
