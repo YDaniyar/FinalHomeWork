dad6fb7a79ad696234fc57ba5496f2b58c9b02fa
Yeren Daniyar <y.daniyar.official@gmail.com>
2024-02-29 00:01:23 +0600

    Task#3

---
+++ Task#3/Program.cs
@@ -0,0 +1,7 @@
+﻿graph TD;
+    A[Начало] --> B["Введите элементы массива через запятую:"];
+    B[input] --> C[originalArray = input.Split(',')];
+    C[originalArray] --> D[FilterArray(originalArray)];
+    D[FilterArray(originalArray)] --> E[result];
+    E[result = new string] --> F["Новый массив:"];
+    G[Конец];
